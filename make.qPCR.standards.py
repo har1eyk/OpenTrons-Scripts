7576dd789f05a0948d4970289ffe9eeb2ba94a6c
Harley King <harleyk@gmail.com>
2021-03-02 07:49:26 -0500

    add stds-13-15
    Added three more dilutions beyond std_12 to get at LOD estimate.

--- make.qPCR.standards.py
+++ make.qPCR.standards.py
@@ -12,8 +12,8 @@ metadata = {
 def run(protocol: protocol_api.ProtocolContext):
 
     # LABWARE
-    fuge_rack = protocol.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '4')
-    water_rack = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', '5') 
+    fuge_rack = protocol.load_labware('vwr_24_tuberack_1500ul', '4')
+    # water_rack = protocol.load_labware('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', '5') 
     # tiprack300 = protocol.load_labware('opentrons_96_tiprack_300ul', '8')
     tiprack300 = protocol.load_labware('opentrons_96_filtertiprack_200ul', '8')
     # tiprack20 = protocol.load_labware('opentrons_96_tiprack_20ul', '9')
@@ -33,6 +33,9 @@ def run(protocol: protocol_api.ProtocolContext):
     std_10 = fuge_rack['C4'] # 900ul water
     std_11 = fuge_rack['C5'] # 900ul water
     std_12 = fuge_rack['C6'] # 900ul water
+    std_13 = fuge_rack['D3'] # 900ul water
+    std_14 = fuge_rack['D4'] # 900ul water
+    std_15 = fuge_rack['D5'] # 900ul water
 
     # PIPETTES
     p300 = protocol.load_instrument(
@@ -44,19 +47,21 @@ def run(protocol: protocol_api.ProtocolContext):
      
     # REAGENTS
     # water = water_rack['A1'] # 15mL concial tube with water, filled between 10 and 13mL mark
-    water = fuge_rack['C1']
+    water = fuge_rack['D6']
     pos_control = fuge_rack['A1'] #30-40ul @1uM
-    mmix = fuge_rack['D1'] # 684ul
+    mmix = fuge_rack['D1'] # 1332ul
     
     # LISTS
-    probe_wells=['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A6', 'B6', 'C6', 'D6', 'E6']
-    std_wells = [std_1, std_2, std_3, std_4, std_5, std_6, std_7, std_8, std_9, std_10, std_11, std_12, water]
-    tip_heights = [22.6,21.3,20,18.5,17.1,15.7,14.3,12.9,11.5,9.8,7.8,5.1,0]
+    probe_wells=['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6']
+    std_wells = [std_1, std_2, std_3, std_4, std_5, std_6, std_7, std_8, std_9, std_10, std_11, std_12, std_13, std_14, std_15, water]
+    # tip_heights = [22.6,21.3,20,18.5,17.1,15.7,14.3,12.9,11.5,9.8,7.8,5.1,0]
+    # tip_heights = [25.3,24.1,22.9,21.6,20.3,18.9,17.4,16,14.6,13.2,11.8,10.2,8.3,3,0,0]
+    tip_heights = [26.4,25.2,24,22.8,21.5,20.1,18.7,17.3,15.9,14.5,13.1,11.6,10,8,4.4,0.2]
     
     #### COMMANDS ######
     # Mix, pipette 54ul mm_Sybr, mm_Probe to each well on plate 
     p300.pick_up_tip()
-    p300.mix(3, 200, mmix.bottom(10)) #10mm from bottom
+    p300.mix(3, 200, mmix.bottom(tip_heights[0])) #10mm from bottom
     p300.flow_rate.aspirate = 30
     p300.flow_rate.dispense = 40
     p300.well_bottom_clearance.aspirate = tip_heights[0] #mm 
@@ -66,12 +71,12 @@ def run(protocol: protocol_api.ProtocolContext):
         p300.aspirate(81, mmix) # 18 * 4 = 72 + 9 =81ul
         protocol.delay(seconds=3) #tip equilibrate
         p300.move_to(mmix.bottom(35)) # excess tip fluid condense 
-        protocol.delay(seconds=1) #tip equilibrate
+        protocol.delay(seconds=3) #tip droplets slide
         p300.touch_tip()
-        protocol.delay(seconds=1) #tip residue form outside droplet
-        p300.move_to(mmix.bottom(38)) # move to center, don't catch front lip of tube
-        protocol.delay(seconds=3) #tip residue form outside droplet
-        p300.touch_tip()
+        # protocol.delay(seconds=1) #tip residue form outside droplet
+        # p300.move_to(mmix.bottom(38)) # move to center, don't catch front lip of tube
+        # protocol.delay(seconds=3) #tip residue form outside droplet
+        # p300.touch_tip()
         p300.dispense(81, stds_plate[well])
         p300.touch_tip()
     p300.drop_tip()
@@ -96,7 +101,8 @@ def run(protocol: protocol_api.ProtocolContext):
     for i in range(len(std_wells)-2): #don't want water tube to be included
         p300.well_bottom_clearance.aspirate = 15 #mm come up for mixing 
         p300.well_bottom_clearance.dispense = 15 #mm 
-        if i==0 or i==10: # first or last tube
+        last_std_tube = len(std_wells)-2 # (int) position of last std tube; last tube = water
+        if i==0 or i==last_std_tube: # first or last tube
             p300.mix(3, 200, std_wells[i]) # need to add mixes to first and last tubes
         p300.mix(3, 200, std_wells[i])
         # p300.well_bottom_clearance.aspirate = 10 #mm 
@@ -117,13 +123,13 @@ def run(protocol: protocol_api.ProtocolContext):
     p300.flow_rate.dispense = 92.86 #default
     p300.drop_tip()
 
-    # add 6ul stds to SYBR, PROBE mmxs into plate wells and dispense into neighboring wells
+    # add pos control stds to PROBE mmxs into plate wells and dispense into neighboring wells
     p20.flow_rate.aspirate = 7.56 #default
     p20.flow_rate.dispense = 7.56 #default
     for i in range(len(std_wells)): #loop 13x, water tube last
         p20.pick_up_tip()
-        p300.pick_up_tip()
-        # move 6ul from std tube to well
+        p300.pick_up_tip() #double barrel
+        # move pos control std tube to well
         p20.well_bottom_clearance.aspirate = 15 
         p20.aspirate(9, std_wells[i]) # transer 8ul from oligo mix to mmix spot
         p20.touch_tip() 
