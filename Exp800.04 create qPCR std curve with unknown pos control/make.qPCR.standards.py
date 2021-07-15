ef1001eb55e1e6c965068a0bfdc2b380f2a1691f
Harley King <harleyk@gmail.com>
2021-07-15 14:53:25 -0400

    remove std dilution creation
    moving the standard dilution creation to its own routine.

--- Exp800.04 create qPCR std curve with unknown pos control/make.qPCR.standards.py
+++ Exp800.04 create qPCR std curve with unknown pos control/make.qPCR.standards.py
@@ -19,24 +19,9 @@ def run(protocol: protocol_api.ProtocolContext):
     # tiprack20 = protocol.load_labware('opentrons_96_tiprack_20ul', '9')
     tiprack20 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '9')
     tempdeck = protocol.load_module('tempdeck', '10')
-    stds_plate = tempdeck.load_labware('amplifyt_96_aluminumblock_300ul')
+    # stds_plate = tempdeck.load_labware('amplifyt_96_aluminumblock_300ul')
+    stds_plate = tempdeck.load_labware('abi_96_wellplate_250ul')
     
-    std_1 = fuge_rack['A3'] # e.g. 10nM pos control DNA, 990ul Water
-    std_2 = fuge_rack['A4'] # 900ul water
-    std_3 = fuge_rack['A5'] # 900ul water
-    std_4 = fuge_rack['A6'] # 900ul water
-    std_5 = fuge_rack['B3'] # 900ul water
-    std_6 = fuge_rack['B4'] # 900ul water 
-    std_7 = fuge_rack['B5'] # 900ul water
-    std_8 = fuge_rack['B6'] # 900ul water
-    std_9 = fuge_rack['C3'] # 900ul water
-    std_10 = fuge_rack['C4'] # 900ul water
-    std_11 = fuge_rack['C5'] # 900ul water
-    std_12 = fuge_rack['C6'] # 900ul water
-    std_13 = fuge_rack['D3'] # 900ul water
-    std_14 = fuge_rack['D4'] # 900ul water
-    std_15 = fuge_rack['D5'] # 900ul water
-
     # PIPETTES
     p300 = protocol.load_instrument(
         'p300_single_gen2', 'left', tip_racks=[tiprack300]
@@ -45,18 +30,32 @@ def run(protocol: protocol_api.ProtocolContext):
         'p20_single_gen2', 'right', tip_racks=[tiprack20]
     )
      
-    # REAGENTS
-    # water = water_rack['A1'] # 15mL concial tube with water, filled between 10 and 13mL mark
-    water = fuge_rack['D6']
-    pos_control = fuge_rack['A1'] #30-40ul @1uM
-    mmix = fuge_rack['D1'] # 1332ul
+    # REAGENTS 
+    std_1 = fuge_rack['A1'] # e.g. 10nM pos control DNA, 990ul Water
+    std_2 = fuge_rack['A2'] # 900ul water
+    std_3 = fuge_rack['A3'] # 900ul water
+    std_4 = fuge_rack['A4'] # 900ul water
+    std_5 = fuge_rack['A5'] # 900ul water
+    std_6 = fuge_rack['A6'] # 900ul water 
+    std_7 = fuge_rack['B1'] # 900ul water
+    std_8 = fuge_rack['B2'] # 900ul water
+    std_9 = fuge_rack['B3'] # 900ul water
+    std_10 = fuge_rack['B4'] # 900ul water
+    std_11 = fuge_rack['B5'] # 900ul water
+    std_12 = fuge_rack['B6'] # 900ul water
+    std_13 = fuge_rack['C1'] # 900ul water
+    std_14 = fuge_rack['C2'] # 900ul water
+    std_15 = fuge_rack['C3'] # 900ul water
+    # pos_control = fuge_rack['D1'] # pos control @1uM
+    mmix = fuge_rack['D6'] # 1332ul
+    water = fuge_rack['D5'] # 1500ul water
     
     # LISTS
     probe_wells=['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6']
     std_wells = [std_1, std_2, std_3, std_4, std_5, std_6, std_7, std_8, std_9, std_10, std_11, std_12, std_13, std_14, std_15, water]
     # tip_heights = [22.6,21.3,20,18.5,17.1,15.7,14.3,12.9,11.5,9.8,7.8,5.1,0]
     # tip_heights = [25.3,24.1,22.9,21.6,20.3,18.9,17.4,16,14.6,13.2,11.8,10.2,8.3,3,0,0]
-    tip_heights = [26.4,25.2,24,22.8,21.5,20.1,18.7,17.3,15.9,14.5,13.1,11.6,10,8,4.4,0.2]
+    tip_heights = [26.4,25.2,24,22.8,21.5,20.1,18.7,17.3,15.9,14.5,13.1,11.6,10,8,4.4,1]
     
    
     #### COMMANDS ######
@@ -85,45 +84,6 @@ def run(protocol: protocol_api.ProtocolContext):
     p300.flow_rate.dispense = 92.86 #reset to default
     p300.well_bottom_clearance.aspirate = 10 #mm default
     
-    ## Make std dilution series      
-    # Make 10nM pos control
-    p20.transfer(
-        10,
-        pos_control.bottom(20), #1uM
-        std_1.bottom(20),
-        mix_after=(2, 20), # remove residual fluid from tip
-        blow_out=True,
-        touch_tip=True,
-        blowout_location='destination well'
-    )
-   
-    # serial dilutions in microfuge tubes, 10% diliutions
-    p300.pick_up_tip()
-    for i in range(len(std_wells)-2): #don't want water tube to be included
-        p300.well_bottom_clearance.aspirate = 15 #mm come up for mixing 
-        p300.well_bottom_clearance.dispense = 15 #mm 
-        last_std_tube = len(std_wells)-2 # (int) position of last std tube; last tube = water
-        if i==0 or i==last_std_tube: # first or last tube
-            p300.mix(3, 200, std_wells[i]) # need to add mixes to first and last tubes
-        p300.mix(3, 200, std_wells[i])
-        # p300.well_bottom_clearance.aspirate = 10 #mm 
-        p300.flow_rate.aspirate = 40 #slow aspirate; no air
-        p300.aspirate(100, std_wells[i])
-        p300.touch_tip()
-        p300.flow_rate.dispense = 40 #slow dispense
-        p300.dispense(100, std_wells[i+1])
-        p300.flow_rate.aspirate = 92.86 #default
-        p300.flow_rate.dispense = 92.86 #default
-        p300.mix(3, 200, std_wells[i+1]) #remove residual inside tip
-        p300.move_to(std_wells[i+1].bottom(15)) #come up for blowout
-        p300.blow_out()
-        protocol.delay(seconds=2) #wait for bubbles to subside
-    p300.well_bottom_clearance.dispense = 1 #mm default
-    p300.well_bottom_clearance.aspirate = 1 #mm default
-    p300.flow_rate.aspirate = 92.86 #default
-    p300.flow_rate.dispense = 92.86 #default
-    p300.drop_tip()
-
     # add pos control stds to PROBE mmxs into plate wells and dispense into neighboring wells
     p20.flow_rate.aspirate = 7.56 #default
     p20.flow_rate.dispense = 7.56 #default
