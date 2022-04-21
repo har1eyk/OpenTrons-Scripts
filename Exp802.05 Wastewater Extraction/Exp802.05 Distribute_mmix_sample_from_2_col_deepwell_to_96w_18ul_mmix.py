da3c81c5f5cbbfa285fc81cd00597cfafc3cbf9d
Harley King <harleyk@gmail.com>
2022-04-21 11:00:19 -0400

    Update Exp802.05 Distribute_mmix_sample_from_2_col_deepwell_to_96w_18ul_mmix.py

--- Exp802.05 Wastewater Extraction/Exp802.05 Distribute_mmix_sample_from_2_col_deepwell_to_96w_18ul_mmix.py
+++ Exp802.05 Wastewater Extraction/Exp802.05 Distribute_mmix_sample_from_2_col_deepwell_to_96w_18ul_mmix.py
@@ -3,9 +3,9 @@ from opentrons import protocol_api
 
 # metadata
 metadata = {
-    'protocolName': 'Distribute Mmix and Wastewater Samples from 96w Deepwell to 96w Plate.',
+    'protocolName': 'Distribute Mmix and Wastewater Samples from 2 Col on Deepwell to 96w Plate.',
     'author': 'Harley King <harley.king@luminultra.com>',
-    'description': 'Aliquoting SARS-CoV-2 mix to plate and then adding 2ul sample extraction after 18ul mix from 96w DeepWell plate.',
+    'description': 'Aliquoting SARS-CoV-2 mix to PCR plate and adding 2ul sample extraction from 2 col deepwell plate after 18ul mix.',
     'apiLevel': '2.11'
 }
 
@@ -40,7 +40,7 @@ def run(protocol: protocol_api.ProtocolContext):
 
     # LABWARE
     fuge_rack = protocol.load_labware('opentrons_24_tuberack_eppendorf_2ml_safelock_snapcap', '11')
-    sample_rack = protocol.load_labware('vwr_24_tuberack_1500ul', '4')
+    # sample_rack = protocol.load_labware('vwr_24_tuberack_1500ul', '4')
     tiprack20 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '9')
     tempdeck = protocol.load_module('tempdeck', '10')
     pcr_plate = tempdeck.load_labware('abi_96_wellplate_250ul')
@@ -48,26 +48,16 @@ def run(protocol: protocol_api.ProtocolContext):
 
     # PIPETTES
     p20 = protocol.load_instrument(
-        'p20_single_gen2', 'right', tip_racks=[tiprack20]
+    'p20_single_gen2', 'right', tip_racks=[tiprack20]
     )
     
     # REAGENTS   
     LU_Mix = fuge_rack['A1'] # LU MasterMix
-
-    sample_1 = sample_rack['A1']
-    sample_2 = sample_rack['A2']
-    sample_3 = sample_rack['A3']
-    sample_4 = sample_rack['A4']
     
-     # LISTS
+    # LISTS
     rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     tot_ww_plates = [ww_plate1]
     sample_cols = [5, 11]
-    sample_dict = {
-        'D7': sample_1,
-        'D12': sample_2,
-        'H7': sample_3,
-        'H12': sample_4}
 
     # #### COMMANDS ######    
     # aspirate mmix to all wells in 96w plate; 18*96 = 1.1=1584
@@ -100,7 +90,8 @@ def run(protocol: protocol_api.ProtocolContext):
                 dest4 = row + str(col-1) #A1, #A2, #A3
                 dest5 = row + str(col) #A1, #A2, #A3
                 dest6 = row + str(col+1) #A1, #A2, #A3
-                p20.aspirate(15, ww_plate[source].bottom(1), rate=0.75) #2*6+3bolus = 15
+                p20.aspirate(15, ww_plate[source].bottom(2), rate=0.75) #2*6+3bolus = 15
+                p20.move_to(ww_plate[source].bottom(3)) #avoid tip jamming bottom unable to aspirate
                 protocol.delay(seconds=2) #equilibrate
                 p20.touch_tip()
                 p20.dispense(2, pcr_plate[dest1].bottom(1))
