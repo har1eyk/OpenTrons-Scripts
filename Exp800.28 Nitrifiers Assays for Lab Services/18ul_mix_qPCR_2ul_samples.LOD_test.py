272216400b0c9f9a7bde541d4835eb3bd34ac7d4
Harley King <harleyk@gmail.com>
2023-02-07 15:57:12 -0500

    adding nitrifier assays
    These assays are to test nitrifiers in lab services.

---
+++ Exp800.28 Nitrifiers Assays for Lab Services/18ul_mix_qPCR_2ul_samples.LOD_test.py
@@ -0,0 +1,146 @@
+# imports
+from opentrons import protocol_api
+
+# metadata
+metadata = {
+    'protocolName': 'Add 18ul Mix and 2ul Sample to Test LOD Using Opt Fwd and Rev and Prove Concentrations. No std curve.',
+    'author': 'Harley King <harley.king@luminultra.com>',
+    'description': 'Creates a 96w plate of optimal primer and probe concentrations to qPCR samples. No std curve is created.',
+    'apiLevel': '2.12'
+}
+##########################
+# functions
+# calculates ideal tip height for entering liquid
+def tip_heights(init_vol, steps, vol_dec):
+    vols = []
+    heights = []
+    # these values originate from Excel spreadsheet "Exp803..."
+    p0=0.029502064
+    p1=0.084625954
+    p2=-0.000174864
+    p3=2.18373E-07
+    p4=-1.30599E-10
+    p5=2.97839E-14
+    if init_vol > 1499:
+        offset = 14 # model out of range; see sheet
+    else:
+        offset = 7 #mm Need to add offset to ensure tip reaches below liquid level
+    for i in range(steps):
+        x = init_vol-vol_dec*i
+        vols.append(x)
+        h = p5*x**5+p4*x**4+p3*x**3+p2*x**2+p1*x**1 + p0
+        h = h-offset
+        if h < 8: # prevent negative heights; go to bottom to avoid air aspirant above certain height
+            h = 1        
+            heights.append(h)
+        else:
+            heights.append(round(h, 1))
+    return heights
+
+def tip_heightsEpp(init_vol, steps, vol_dec):
+    vols = []
+    heights = []
+    # these values originate from Excel spreadsheet "Exp803..."
+    p0=-0.272820744
+    p1=0.019767959
+    p2=2.00442E-06
+    p3=-8.99691E-09
+    p4=6.72776E-12
+    p5=-1.55428E-15
+    if init_vol > 2000:
+        offset = 12 # model out of range; see sheet
+    else:
+        offset = 11 #mm Need to add offset to ensure tip reaches below liquid level
+    for i in range(steps):
+        x = init_vol-vol_dec*i
+        vols.append(x)
+        h = p5*x**5+p4*x**4+p3*x**3+p2*x**2+p1*x**1 + p0
+        h = h-offset
+        if h < 8: # prevent negative heights; go to bottom to avoid air aspirant above certain height
+            h = 0        
+            heights.append(h)
+        else:
+            heights.append(round(h, 1))
+    return heights
+
+def run(protocol: protocol_api.ProtocolContext):
+
+    # LABWARE
+    mix_rack = protocol.load_labware('eppendorf_24_tuberack_2000ul', '7')
+    fuge_rack = protocol.load_labware('vwr_24_tuberack_1500ul', '2')
+    # tiprack300 = protocol.load_labware('opentrons_96_filtertiprack_200ul', '8')
+    tiprack20 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '9')
+    tempdeck = protocol.load_module('tempdeck', '10')
+    # plate = tempdeck.load_labware('amplifyt_96_aluminumblock_300ul')
+    plate = tempdeck.load_labware('bioer_96_aluminumblock_200ul')
+
+    # PIPETTES
+    # p300 = protocol.load_instrument(
+    #     'p300_single_gen2', 'left', tip_racks=[tiprack300]
+    # )
+    p20 = protocol.load_instrument(
+        'p20_single_gen2', 'right', tip_racks=[tiprack20]
+    )
+   # mix_rack
+    mmix = mix_rack['D1'] # 96*18*1.2 = 2073.6 
+    # WATER = mix_rack['A1'] # 1500ul WATER
+       
+    #fuge_rack
+    samp_1 = fuge_rack['B1'] # e.g. 0.625uM # empty
+    samp_2 = fuge_rack['B2'] # e.g. 1.25uM # empty
+    samp_3 = fuge_rack['D1'] # e.g. 2.5uM # empty
+    samp_4 = fuge_rack['D2'] # e.g. 5.0uM # empty
+    samp_5 = fuge_rack['D3'] # e.g. 7.5uM # empty
+    samp_6 = fuge_rack['D4'] # e.g. 10uM # empty
+    samp_7 = fuge_rack['D5'] # e.g. 10uM # empty
+    samp_8 = fuge_rack['B3'] # e.g. 10uM # empty
+    
+    # user inputs
+    rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    samp_sources = [samp_1, samp_2, samp_3, samp_4, samp_5, samp_6, samp_7, samp_8]
+    
+    # ### COMMANDS ######
+    # OVERVIEW
+    # 1.1 add 18ul MMIX to 96 wells
+    # 1.2 2ul added to every sample, row-by-row
+    
+    # 1.1 add 18ul MMIX to 96 wells
+    p20.pick_up_tip()
+    tubeH = tip_heightsEpp(1800, 96, 18)
+    h_counter = 0
+    p20.mix(2, 20, mmix.bottom(tubeH[h_counter]))
+    for row in rows:
+        for col in range(12):
+            p20.aspirate(18, mmix.bottom(tubeH[h_counter]))
+            protocol.delay(seconds=1) #equilibrate
+            # p20.move_to(mmix.bottom(tubeH[h_counter]+10)) # suspend above mix; droplets coalesce
+            p20.move_to(mmix.top(-4)) # suspend above mix; droplets coalesce
+            protocol.delay(seconds=2)
+            p20.touch_tip(v_offset=-4, speed=40)
+            # p20.move_to(mmix.bottom(tubeH[h_counter])) # remove fluid my dipping in mix
+            dest = row + str(col+1)
+            p20.dispense(18, plate[dest].bottom(3))
+            p20.blow_out(plate[dest].bottom(8))
+            p20.move_to(plate[dest].bottom(3)) # remove traces of mmix
+            h_counter +=1
+    p20.drop_tip()
+    
+    # 1.2 2ul added to every sample, row-by-row
+    for sample, row in zip(samp_sources, rows):
+        for col in range(0,12,6):
+            p20.pick_up_tip()
+            p20.mix(1,20, sample.bottom(2)) # pre-wetting tip is more accurate
+            p20.aspirate(18, sample.bottom(2))
+            destOne = row + str(col+1)
+            destTwo = row + str(col+2)
+            destThree = row + str(col+3)
+            destFour = row + str(col+4)
+            destFive = row + str(col+5)
+            destSix = row + str(col+6)
+            p20.dispense(2, plate[destOne].bottom(2))
+            p20.dispense(2, plate[destTwo].bottom(2))
+            p20.dispense(2, plate[destThree].bottom(2))
+            p20.dispense(2, plate[destFour].bottom(2))
+            p20.dispense(2, plate[destFive].bottom(2))
+            p20.dispense(2, plate[destSix].bottom(2))
+            p20.drop_tip()
---
+++ Exp800.28 Nitrifiers Assays for Lab Services/2_masterMixes_create.pos.control.amplification.series.Std_1-8_18ul_mix.py
@@ -0,0 +1,162 @@
+# imports
+from opentrons import protocol_api
+
+# metadata
+metadata = {
+    'protocolName': 'Compare Two MasterMixes, 18ul, Using 8 Standards',
+    'author': 'Harley King <harley.king@luminultra.com>',
+    'description': 'Create a 4-rep pos control amplification in a 96w plate with 18ul from 2 mastermixes.',
+    'apiLevel': '2.12'
+}
+##########################
+# functions
+# returns zipped array of volumes and heights for
+# simultaneous dispensing and tip ascension
+# to reduce chance of air pocket formation
+def d_dispense(vol:int, steps:int, height:float):
+    # what is the height of total volume?
+    lowestHeight = 1 # don't go lower than this
+    heightIncrement = (height-lowestHeight)/steps
+    heightList = list(range(1,steps)) # make a list from 1..steps
+    heightArray =[1] + [1+x*heightIncrement for x in heightList]
+    volArray = [vol/steps]*steps
+    return zip(volArray, heightArray)
+
+# calculates ideal tip height for entering liquid
+def tip_heights(init_vol, steps, vol_dec):
+    vols = []
+    heights = []
+    # these values originate from Excel spreadsheet "Exp803..."
+    p0=0.029502064
+    p1=0.084625954
+    p2=-0.000174864
+    p3=2.18373E-07
+    p4=-1.30599E-10
+    p5=2.97839E-14
+    if init_vol > 1499:
+        offset = 14 # model out of range; see sheet
+    else:
+        offset = 7 #mm Need to add offset to ensure tip reaches below liquid level
+    for i in range(steps):
+        x = init_vol-vol_dec*i
+        vols.append(x)
+        h = p5*x**5+p4*x**4+p3*x**3+p2*x**2+p1*x**1 + p0
+        h = h-offset
+        if h < 8: # prevent negative heights; go to bottom to avoid air aspirant above certain height
+            h = 1        
+            heights.append(h)
+        else:
+            heights.append(round(h, 1))
+    return heights
+
+def run(protocol: protocol_api.ProtocolContext):
+
+    # LABWARE
+    mix_rack = protocol.load_labware('vwr_24_tuberack_1500ul', '1')
+    fuge_rack = protocol.load_labware('vwr_24_tuberack_1500ul', '2')
+    tiprack300 = protocol.load_labware('opentrons_96_filtertiprack_200ul', '8')
+    tiprack20 = protocol.load_labware('opentrons_96_filtertiprack_20ul', '9')
+    tempdeck = protocol.load_module('tempdeck', '10')
+    # stds_plate = tempdeck.load_labware('abi_96_wellplate_250ul')
+    stds_plate = tempdeck.load_labware('bioer_96_aluminumblock_200ul')
+    
+    # PIPETTES
+    p300 = protocol.load_instrument(
+        'p300_single_gen2', 'left', tip_racks=[tiprack300]
+    )
+    p20 = protocol.load_instrument(
+        'p20_single_gen2', 'right', tip_racks=[tiprack20]
+    )
+     
+    # REAGENTS 
+    std_1 = fuge_rack['A1'] # diluted standard
+    std_2 = fuge_rack['A2']
+    std_3 = fuge_rack['A3']
+    std_4 = fuge_rack['A4']
+    std_5 = fuge_rack['A5']
+    std_6 = fuge_rack['A6'] 
+    std_7 = fuge_rack['B1']
+    std_8 = fuge_rack['B2']
+
+    # pos_control = fuge_rack['D1'] # pos control @1uM
+    water = fuge_rack['D6'] # 1500ul water
+    
+    # mix_rack
+    mmixOne = mix_rack['A1'] # 18*4*(1.125)*8*1.1 = 712.8µl
+    mmixTwo = mix_rack['A6'] # 
+    # CALCS
+    mix_per_well = 18 #how much volume (ul) mastermix should be in each well?
+    overage = 0.125 # e.g. twenty percent waste as decimal
+    reps = 4 #how many replicates of each std?
+    multiwell_mix = mix_per_well*reps*(1+overage) # 18*4*(1+0.125) = 81ul
+    sample_per_well = 2
+    multisample_mix = sample_per_well*reps*(1+overage) # 2*4*(1+0.125) = 9
+    # LISTS
+    mixOne_wells=['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1']
+    mixTwo_wells=['A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6']
+    std_wells = [std_1, std_2, std_3, std_4, std_5, std_6, std_7, std_8]
+    mixList = [mmixOne, mmixTwo]
+    wellList = [mixOne_wells, mixTwo_wells]
+
+    #### COMMANDS ######
+    # Mix, pipette mastermix containing probe to each well on plate 
+    mmixH = multiwell_mix * len(mixOne_wells)*1.1 # 18*4*(1+0.125) = 81 *8 = 648 + 1.1 = 712.8
+    for mmix, wwell in zip(mixList, wellList): # iterate through mastermixes and well list per mix
+        p300.pick_up_tip()
+        p300.mix(3, 200, mmix.bottom(10)) #first height
+        for wellLoc, h in zip(wwell, tip_heights(mmixH, len(std_wells), multiwell_mix)): # iterate through wells and heights for each mix
+            p300.aspirate(multiwell_mix, mmix.bottom(h), rate=0.75) # 15 * 4 (1.125)= 67.5
+            protocol.delay(seconds=2) #tip equilibrate
+            p300.move_to(mmix.bottom(35)) # excess tip fluid condense 
+            protocol.delay(seconds=2) #tip droplets slide
+            p300.move_to(mmix.bottom(h)) # excess tip fluid condense 
+            p300.touch_tip(v_offset=-4) #touch 4 mm below surface
+            for volD, heightD in d_dispense(multiwell_mix, 6, 6):
+                p300.dispense(volD, stds_plate[wellLoc].bottom(heightD))
+            # p300.dispense(multiwell_mix, stds_plate[well])
+            p300.blow_out(stds_plate[wellLoc].bottom(12))
+            p300.move_to(stds_plate[wellLoc].bottom(2))
+            p300.touch_tip()
+        p300.drop_tip()
+    
+    # # add pos control stds to into plate wells and dispense into neighboring wells
+    for mmix, wwell in zip(mixList, wellList): # iterate through mastermixes and well list per mix for pos control addition and dispense
+        for i in range(len(std_wells)): #loop 13x, water tube last
+            p20.pick_up_tip()
+            p300.pick_up_tip() #double barrel
+            p20.aspirate(multisample_mix, std_wells[i].bottom(4)) #aspirate 22.5ul from template into mix well
+            p20.move_to(std_wells[i].bottom(30))
+            protocol.delay(seconds=3) #coalescing step
+            p20.move_to(std_wells[i].bottom(18)) # remove fluid from tip
+            p20.dispense(multisample_mix, stds_plate[wwell[i]].bottom(2))
+            p20.mix(4, 10, stds_plate[wwell[i]].bottom(1), rate=0.5) # can't be mixed homogenously with p20 #ivetried
+            p20.move_to(stds_plate[wwell[i]].bottom(12)) #above mmix solution
+            protocol.delay(seconds=2) #outside fluid coalesce 
+            p20.blow_out(stds_plate[wwell[i]].bottom(12))
+            p20.move_to(stds_plate[wwell[i]].bottom(2)) 
+            p20.move_to(stds_plate[wwell[i]].bottom(30))  # above plate so tips don't crash Smoothie error?
+            
+            # mix with p300
+            p300.move_to(stds_plate[wwell[i]].bottom(30)) # above plate so tips don't crash Smoothie error?
+            p300.mix(3, 60, stds_plate[wwell[i]].bottom(3)) # mix 90ul mmix with dna
+            p300.blow_out(stds_plate[wwell[i]].bottom(12))
+            protocol.delay(seconds=1)
+            p300.touch_tip(stds_plate[wwell[i]], v_offset=-2, speed=20)
+            p300.move_to(stds_plate[wwell[i]].bottom(30))
+            # transfer to adjacent wells
+            for x in range(1,5): # need int 1, 2, 3 and 4.
+                p20.move_to(stds_plate[wwell[i]].bottom(30))
+                p20.aspirate(20, stds_plate[wwell[i]].bottom(1), rate=0.75) # asp from 54ul, dispense to neighbor well
+                protocol.delay(seconds=2) #equilibrate
+                # find digits in well, A1 and A10 and puts into list
+                findNums = [int(i) for i in wwell[i].split()[0] if i.isdigit()]
+                # joins nums from list [1, 0] -> 10 type = string
+                colNum = ''.join(map(str, findNums))
+                # this finds row
+                row = wwell[i].split()[0][0]
+                # put it all together into a destination well
+                dest = row+str(int(colNum)+x) # row + neighbor well i.e. 1, 2
+                p20.dispense(20, stds_plate[dest].bottom(1), rate=0.75)
+                p20.touch_tip()
+            p300.drop_tip()
+            p20.drop_tip()
